fca7383781cebf7794db76d0ce35f649030080cb
Gulum <gulumkanusonkyzy@gmail.com>
2024-01-29 20:06:48 +0600

    JDBC

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
@@ -1,7 +1,12 @@
 package peaksoft;
 
+import peaksoft.service.UserService;
+import peaksoft.service.UserServiceImpl;
+import peaksoft.util.Util;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        System.out.println("klajsfdlk");
     }
 }
--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/dao/UserDaoJdbcImpl.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/dao/UserDaoJdbcImpl.java
@@ -1,36 +1,117 @@
 package peaksoft.dao;
 
 import peaksoft.model.User;
+import peaksoft.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class UserDaoJdbcImpl implements UserDao {
+  private final Connection connection = Util.getConnection();
 
     public UserDaoJdbcImpl() {
 
     }
 
     public void createUsersTable() {
+        String query = """
+    create table if not exists users(
+    id serial primary key,
+    first_name varchar,
+    last_name varchar,
+    age int);
+    """;
+
+     int execute = 0;
+     try {
+       Statement statement = connection.createStatement();
+       execute = statement.executeUpdate(query);
+       statement.close();
+     }catch (SQLException e){
+       System.out.println(e.getMessage());
+     }
+      System.out.println(execute != 0 ? "Success":"Error");
 
     }
 
     public void dropUsersTable() {
+      String query = "drop table users";
+      try{
+        Statement statement = connection.createStatement();
+        statement.execute(query);
+        System.out.println("Deleted table");
+        statement.close();
+      }catch (SQLException e){
+        System.out.println(e.getMessage());
+      }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+    String query = """
+            insert into users(first_name, last_name,age)
+            values(?, ?, ?)
+            """;
+      int execute = 0;
+      try{
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1,name);
+        preparedStatement.setString(2,lastName);
+        preparedStatement.setInt(3,age);
+        execute = preparedStatement.executeUpdate();
+      }catch (SQLException e){
+        System.out.println(e.getMessage());
+      }
+      System.out.println(execute != 0 ? name + "Successfully updated":"Error ");
     }
 
     public void removeUserById(long id) {
+      String query = "delete from users where id = ? ";
+      try{
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setLong(1,id);
 
+        preparedStatement.execute();
+        System.out.println("Deleted!");
+        preparedStatement.close();
+      }catch (SQLException e ){
+        System.out.println(e.getMessage());
+      }
     }
 
     public List<User> getAllUsers() {
-        return null;
+      List<User> users = new ArrayList<>();
+
+      String query = "select * from users";
+      try {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+        while (resultSet.next()){
+          User user = new User();
+          user.setId(resultSet.getLong("id"));
+          user.setName(resultSet.getString("first_name"));
+          user.setLastName(resultSet.getString("last_name"));
+          user.setAge(resultSet.getByte("age"));
+          users.add(user);
+        }
+      }catch (SQLException e){
+        System.out.println(e.getMessage());
+      }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+      String query = "truncate table users";
+      int execute = 0;
+      try{
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        execute = preparedStatement.executeUpdate();
+        preparedStatement.close();
+      }catch (SQLException e){
+        System.out.println(e.getMessage());
+      }
+      System.out.println(execute != 0 ? "Cleaned table" : "Error");
     }
 }
